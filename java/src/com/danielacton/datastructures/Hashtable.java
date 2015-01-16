d963671409e42192689aabb0ac504d5f6ae67b6b
Daniel Acton <code@danielacton.com>
2015-01-16 10:55:44 +0200

    first commit

---
+++ java/src/com/danielacton/datastructures/Hashtable.java
@@ -0,0 +1,245 @@
+package com.danielacton.datastructures;
+
+public class Hashtable {
+  private final int tableSize = 20;
+  private int numElements;
+  private Object[] table;
+
+  public Hashtable() {
+    this.table = new Object[this.tableSize];
+    this.numElements = 0;
+  }
+
+  public Hashtable(Object initialKey, Object initialData) {
+    this();
+    this.add(initialKey, initialData);
+  }
+
+  public Hashtable(Object[] initialKeys, Object[] initialData) {
+    this();
+
+    // Don't do stuff with differently sized input arrays
+    if (initialKeys.length != initialData.length) {
+      System.err
+          .println("ERROR: The size of the keys and data arrays are different");
+    }
+
+    this.add(initialKeys, initialData);
+  }
+
+  /**
+   * Add an item to the hash table.
+   * 
+   * @param key The key for the data.
+   * @param data The data to store.
+   */
+  public void add(Object key, Object data) {
+    if (data == null || key == null) {
+      System.err.println("ERROR: Either the key or the data are null");
+      return;
+    }
+
+    /* Don't add duplicate keys */
+    if (this.contains(key)) {
+      return;
+    }
+
+    /* Find out where in our array should the item go */
+    int position = this.hash(key);
+
+    /* If nothing exists in the position, create a new linked list there */
+    if (this.table[position] == null) {
+      this.table[position] = new LinkedList();
+    }
+
+    /* Add to the linked list in the appropriate position */
+    ((LinkedList) this.table[position]).add(new HashtableNode(key, data));
+    this.numElements++;
+  }
+
+  /**
+   * Add a an array of items to the hash table with corresponding keys. The
+   * given arrays of keys and data must have corresponding indexes. So if you
+   * have a key at index 2 in the input key array, the data at index 2 of the
+   * input data array is what will be put in the Hasthable.
+   * 
+   * @param keys The keys for the data.
+   * @param inputData The data to add.
+   */
+  public void add(Object[] keys, Object[] inputData) {
+    for (int i = 0; i < inputData.length; i++) {
+      this.add(keys[i], inputData[i]);
+    }
+  }
+
+  /**
+   * Remove an item from the hashtable
+   * 
+   * @param key The item to remove
+   */
+  public void remove(Object key) {
+    int hashVal = this.hash(key);
+
+    if (this.table[hashVal] != null) {
+      HashtableNode node = new HashtableNode();
+      node.setKey(key);
+
+      if (((LinkedList) this.table[hashVal]).indexOf(node) > -1) {
+        ((LinkedList) this.table[hashVal]).remove(node);
+        this.numElements--;
+      }
+    }
+  }
+
+  /**
+   * Remove many items from the hashtable.
+   * 
+   * @param inputData The array of items to remove.
+   */
+  public void remove(Object[] keys) {
+    for (int i = 0; i < keys.length; i++) {
+      this.remove(keys[i]);
+    }
+  }
+
+  /**
+   * Calculate the hash for an object being stored. Use the toString() method to
+   * get a string from the Object and then add the ASCII values of the string
+   * (unless it's a-f, in which case we'll use it as hex).
+   * 
+   * @param key The key for which to compute a hash.
+   * @return The hash, an integer to be used for the data's position in the
+   *         backing array.
+   */
+  private int hash(Object key) {
+
+    // Start with a base, just so that it's not 0 for empty strings
+    int result = 42;
+
+    String inputString = key.toString().toLowerCase();
+
+    char[] characters = inputString.toCharArray();
+    for (int i = 0; i < characters.length; i++) {
+      char currentChar = characters[i];
+
+      if (currentChar == 'a' || currentChar == 'b' || currentChar == 'c'
+          || currentChar == 'e' || currentChar == 'e' || currentChar == 'f') {
+        result += Integer.parseInt("" + currentChar, 16);
+      }
+
+      int j = (int) currentChar;
+      result += j;
+    }
+
+    return (result % this.tableSize);
+  }
+
+  /**
+   * Give a string representation of the hash table.
+   * 
+   * @return A string representation of the contents of the Hashtable.
+   */
+  public String toString() {
+    StringBuffer buffer = new StringBuffer();
+
+    buffer.append(System.getProperty("line.separator"));
+    buffer.append("{");
+    buffer.append(System.getProperty("line.separator"));
+
+    for (int i = 0; i < this.table.length; i++) {
+      if (this.table[i] != null) {
+        buffer.append("\t" + (LinkedList) this.table[i]);
+        buffer.append(System.getProperty("line.separator"));
+      }
+    }
+
+    buffer.append("}");
+
+    return buffer.toString();
+  }
+
+  /**
+   * Get the number of elements in the Hashtable.
+   * 
+   * @return The number of elements in the Hasthable.
+   */
+  public int getNumElements() {
+    return this.numElements;
+  }
+
+  /**
+   * Check if the table contains a certain element or not.
+   * 
+   * @param data The data to search for.
+   * @return Whether or not the element exists.
+   */
+  public boolean contains(Object key) {
+    boolean result = false;
+    int hash = this.hash(key);
+
+    if (this.table[hash] != null) {
+      HashtableNode node = new HashtableNode();
+      node.setKey(key);
+      if (((LinkedList) this.table[hash]).indexOf(node) > -1) {
+        result = true;
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * An inner class to represent an entry in the Hashtable. Bascially just holds
+   * the key/data pair.
+   * 
+   * @author Daniel Acton (code@danielacton.com)
+   *
+   */
+  private class HashtableNode {
+    private Object key;
+    private Object data;
+
+    public HashtableNode() {
+      this.key = null;
+      this.data = null;
+    }
+
+    public HashtableNode(Object inKey, Object inData) {
+      this.key = inKey;
+      this.data = inData;
+    }
+
+    public Object getData() {
+      return data;
+    }
+
+    public void setData(Object data) {
+      this.data = data;
+    }
+
+    public Object getKey() {
+      return key;
+    }
+
+    public void setKey(Object key) {
+      this.key = key;
+    }
+
+    /*
+     * Equality can be based on key alone because there can't be 2 nodes with
+     * the same key in the table
+     */
+    public boolean equals(Object obj) {
+      if (obj instanceof HashtableNode) {
+        HashtableNode node = (HashtableNode) obj;
+        return this.key.equals(node.getKey());
+      } else {
+        return false;
+      }
+    }
+
+    public String toString() {
+      return "Key: [" + this.key + "] data: [" + this.data + "]";
+    }
+  }
+}
---
+++ java/src/com/danielacton/datastructures/test/LinkedListTest.java
@@ -0,0 +1,279 @@
+package com.danielacton.datastructures.test;
+
+import junit.framework.TestCase;
+
+import com.danielacton.datastructures.LinkedList;
+
+/**
+ * Tests the LinkedList class
+ * @author Daniel Acton
+ *
+ */
+public class LinkedListTest extends TestCase {
+	
+	/**
+	 * Test that the LinkedList is constructed. 
+	 * Result is a list with 0 elements.
+	 */
+	public void testCreation() {
+		LinkedList list = new LinkedList();
+		assertEquals(0, list.size());
+	}
+	
+	/**
+	 * Test that creating with a single element works 
+	 * Result is a list with 1 element with a certain value
+	 */
+	public void testCreationWithOneElement() {
+		LinkedList list = new LinkedList("a");
+		assertEquals(1, list.size());
+		assertEquals("a", list.elementAt(0));
+	}
+	
+	/**
+	 * Test that creating with many elements works 
+	 * Result is a list with 4 elements with certain values
+	 */
+	public void testCreationWithManyElements() {
+		LinkedList list = new LinkedList(new Object [] {"a", "b", "c", "d"});
+
+		assertEquals("a", list.elementAt(0));
+		assertEquals("b", list.elementAt(1));
+		assertEquals("c", list.elementAt(2));
+		assertEquals("d", list.elementAt(3));
+		assertEquals(4, list.size());
+	}
+
+	/**
+	 * Test that adding a single element works
+	 * Result is a list with 1 elements with a certain value
+	 */
+	public void testAddSingle() {
+		LinkedList list = new LinkedList();
+
+		list.add("a");
+		
+		assertEquals("a", list.elementAt(0));
+		assertEquals(1, list.size());
+	}
+
+	/**
+	 * Test that adding many elements works
+	 * Result is a list with many elements with certain values
+	 */
+	public void testAddMany() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z"});
+		
+		assertEquals("x", list.elementAt(0));
+		assertEquals("y", list.elementAt(1));
+		assertEquals("z", list.elementAt(2));
+
+		assertEquals(3, list.size());
+	}
+	
+	/**
+	 * Test that removing a single element from the start of the list works
+	 * Result is a list moved "one to the left" with first item replaced
+	 */
+	public void testRemoveSingleStart() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z"});
+		list.remove("x");
+		
+		assertEquals("y", list.elementAt(0));
+		assertEquals("z", list.elementAt(1));
+		assertEquals(2, list.size());		
+	}
+
+	/**
+	 * Test that removing a single element from anywhere in the list works
+	 * Result is a list moved "one to the left" with item replaced
+	 */
+	public void testRemoveSingleMiddle() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z"});
+		list.remove("y");
+		
+		assertEquals("x", list.elementAt(0));
+		assertEquals("z", list.elementAt(1));
+		assertEquals(2, list.size());		
+	}
+
+	/**
+	 * Test that removing a single element from the end of the list works
+	 * Result is a list that is 1 element shorter
+	 */
+	public void testRemoveSingleEnd() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z"});
+		list.remove("z");
+		
+		assertEquals("x", list.elementAt(0));
+		assertEquals("y", list.elementAt(1));
+		assertEquals(2, list.size());		
+	}
+
+	/**
+	 * Test that removing all elements works
+	 * Result is an empty list
+	 */
+	public void testRemoveAll() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z"});
+		list.remove(new Object [] {"x", "y", "z"});
+		
+		assertEquals(0, list.size());		
+	}
+
+	/**
+	 * Test that removing many elements from the start of the list works
+	 * Result is a list moved "N to the left"
+	 */
+	public void testRemoveManyFromBeginning() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z", "a", "b", "c"});
+		list.remove(new Object [] {"x", "y", "z"});
+		
+		assertEquals("a", list.elementAt(0));
+		assertEquals("b", list.elementAt(1));
+		assertEquals("c", list.elementAt(2));
+		assertEquals(3, list.size());		
+	}
+
+	/**
+	 * Test that removing many elements from the end of the list works
+	 * Result is a list that is N elements shorter
+	 */
+	public void testRemoveManyFromEnd() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z", "a", "b", "c"});
+		list.remove(new Object [] {"a", "b", "c"});
+		
+		assertEquals("x", list.elementAt(0));
+		assertEquals("y", list.elementAt(1));
+		assertEquals("z", list.elementAt(2));
+		assertEquals(3, list.size());		
+	}
+
+	/**
+	 * Test that removing many elements from the end of the list works
+	 * Result is a list that is N elements shorter, keeping start and end bits
+	 */
+	public void testRemoveManyFromInside() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z", "a", "b", "c"});
+		list.remove(new Object [] {"y", "z", "a", "b"});
+		
+		assertEquals("x", list.elementAt(0));
+		assertEquals("c", list.elementAt(1));
+		assertEquals(2, list.size());		
+	}
+	
+	/**
+	 * Test that searching for an element works
+	 * Result is the correct position of the elements added
+	 */
+	public void testIndexOf() {
+		LinkedList list = new LinkedList();
+
+		list.add(new Object [] {"x", "y", "z", "a", "b", "c"});
+		
+		assertEquals(0, list.indexOf("x"));		
+		assertEquals(1, list.indexOf("y"));		
+		assertEquals(2, list.indexOf("z"));		
+		assertEquals(3, list.indexOf("a"));		
+		assertEquals(4, list.indexOf("b"));		
+		assertEquals(5, list.indexOf("c"));		
+		assertEquals(-1, list.indexOf("g"));		
+	}
+
+	/**
+	 * Concatenate lists A and B where A is empty
+	 * Result is B
+	 */
+	public void testConcatenationEmptyA() {
+		LinkedList listA = new LinkedList();
+		LinkedList listB = new LinkedList();
+		
+		listB.add(new Object [] {"x", "y", "z", "a", "b", "c"});
+		
+		listA.concatenate(listB);
+		assertEquals(0, listA.indexOf("x"));		
+		assertEquals(1, listA.indexOf("y"));		
+		assertEquals(2, listA.indexOf("z"));		
+		assertEquals(3, listA.indexOf("a"));		
+		assertEquals(4, listA.indexOf("b"));		
+		assertEquals(5, listA.indexOf("c"));	
+		assertEquals(listA.size(), listB.size());
+	}
+
+	/**
+	 * Concatenate lists A and B where B is empty
+	 * Result is A
+	 */
+	public void testConcatenationEmptyB() {
+		LinkedList listA = new LinkedList();
+		LinkedList listB = new LinkedList();
+		
+		listA.add(new Object [] {"x", "y", "z", "a", "b", "c"});
+		
+		int i = listA.size();
+		
+		listA.concatenate(listB);
+		assertEquals(0, listA.indexOf("x"));		
+		assertEquals(1, listA.indexOf("y"));		
+		assertEquals(2, listA.indexOf("z"));		
+		assertEquals(3, listA.indexOf("a"));		
+		assertEquals(4, listA.indexOf("b"));		
+		assertEquals(5, listA.indexOf("c"));
+		
+		assertEquals(listA.size(), i);
+	}
+
+	/**
+	 * Concatenate lists A and B where neither is empty
+	 * Result is A+B
+	 */
+	public void testConcatenationABNonEmpty() {
+		LinkedList listA = new LinkedList();
+		LinkedList listB = new LinkedList();
+		
+		listA.add(new Object [] {"x", "y", "z"});
+		
+		int i = listA.size();
+		
+		listB.add(new Object [] {"a", "b", "c"});
+		
+		listA.concatenate(listB);
+		assertEquals(0, listA.indexOf("x"));		
+		assertEquals(1, listA.indexOf("y"));		
+		assertEquals(2, listA.indexOf("z"));		
+		assertEquals(3, listA.indexOf("a"));		
+		assertEquals(4, listA.indexOf("b"));		
+		assertEquals(5, listA.indexOf("c"));	
+		
+		assertEquals(listA.size(), listB.size()+i);
+	}
+
+	/**
+	 * Concatenate lists A and B where both are empty
+	 * Result is empty
+	 */
+	public void testConcatenationABBothEmpty() {
+		LinkedList listA = new LinkedList();
+		LinkedList listB = new LinkedList();
+		
+		listA.concatenate(listB);
+		assertEquals(0, listA.size());
+	}
+
+}
